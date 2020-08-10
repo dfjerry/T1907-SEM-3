17b897358b20eace8f553e139a2ebba70493140a
Nguyen Thanh Tuan <tuantuna955@gmail.com>
2020-08-10 12:12:58 +0700

    UWP-Tutorial01

--- App2/App2/App.xaml.cs
+++ App2/App2/App.xaml.cs
@@ -66,7 +66,7 @@ namespace App2
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(Grid), e.Arguments);
                     /*Stack panel*/
                 }
                 // Ensure the current window is active
