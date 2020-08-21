a5d5e63e65dc3b189119a467ef0f2021edc02b64
Nguyen Thanh Tuan <tuantuna955@gmail.com>
2020-08-21 11:48:20 +0700

    weather

---
+++ YoutubeDemoApp/YoutubeDemoApp/VideoPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿
+using MyToolkit.Multimedia;
+using System;
+using System.Net.NetworkInformation;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using YoutubeDemoApp;
+using YoutubeDemoApp.Models;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace YoutubeDemoApp
+{
+  
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class VideoPage : Page
+    {
+        Video video;
+        public VideoPage()
+        {
+            this.InitializeComponent();
+        }
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            try
+            {
+                if (NetworkInterface.GetIsNetworkAvailable())
+                {
+                    video = e.Parameter as Video;
+                    var Url = await YouTube.GetVideoUriAsync(video.Id, YouTubeQuality.Quality1080P);
+                    Console.WriteLine("Day la url: "+Url);
+                    Player.Source = Url.Uri;
+                }
+                else
+                {
+                    MessageDialog message = new MessageDialog("You are not connected to Internet");
+                    await message.ShowAsync();
+                    this.Frame.GoBack();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            base.OnNavigatedTo(e);
+        }
+        private void btnHomePage_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage), new object());
+        }
+    }
+}
